73285103ac0d31eaa5a165c671fe4108628e4974
HudsonFaris <hudsonfaris@gmail.com>
2024-10-28 20:14:06 -0600

    Map Not used and Analytics Tracker

--- script.js
+++ script.js
@@ -34,3 +34,44 @@ contactForm.addEventListener('submit', (e) => {
     // Optionally, display a thank-you message before form submission
     alert('Your email client will now open to send the message.');
 });
+
+// Typing Animation
+const typedTextSpan = document.getElementById('typed-text');
+const cursorSpan = document.getElementById('cursor');
+
+const textArray = [
+    "A driven and ambitious professional.",
+    "Passionate about software engineering.",
+    "Ready to make a meaningful impact."
+];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000; // Delay @test between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        textArrayIndex = (textArrayIndex + 1) % textArray.length;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
